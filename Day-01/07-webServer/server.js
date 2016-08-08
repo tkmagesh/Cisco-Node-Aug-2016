41ce846baf5634b033bdda64d5d71e9604b1879f
Magesh <tkmagesh77@gmail.com>
2016-08-08 15:16:14 +0530

    Day-01 Complete

--- Day-01/07-webServer/server.js
+++ Day-01/07-webServer/server.js
@@ -25,7 +25,7 @@ var server = http.createServer(function(req, res){
 			res.statusCode = 404;
 			res.end();
 		}
-	} else if (urlObj.pathname === '/calculator'){
+	} else if (urlObj.pathname === '/calculator' && req.method === 'GET'){
 		var reqData = querystring.parse(urlObj.query);
 		var op = reqData.op,
 			n1 = parseInt(reqData.n1, 10),
@@ -34,6 +34,20 @@ var server = http.createServer(function(req, res){
 		res.write(result.toString());
 		res.end();
 	
+	} else if (urlObj.pathname === '/calculator' && req.method === 'POST'){
+		var temp = '';
+		req.on('data', function(chunk){
+			temp += chunk;
+		});
+		req.on('end', function(){
+			var reqData = querystring.parse(temp);
+			var op = reqData.op,
+				n1 = parseInt(reqData.n1, 10),
+				n2 = parseInt(reqData.n2, 10);
+			var result = calculator[op](n1, n2);
+			res.write(result.toString());
+			res.end();
+		});
 	} else {
 		res.statusCode = 404;
 		res.end();
