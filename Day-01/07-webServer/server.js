33be95519cbab58e6e23b44b71e9bb82dfd8b4ea
Magesh <tkmagesh77@gmail.com>
2016-08-08 14:55:12 +0530

    handling calculator requests

--- Day-01/07-webServer/server.js
+++ Day-01/07-webServer/server.js
@@ -1,19 +1,42 @@
-var http = require('http');
+var http = require('http'),
+	fs = require('fs'),
+	path = require('path'),
+	url = require('url'),
+	querystring = require('querystring'),
+	calculator = require('./calculator');
+
+
+var staticExtns = ['.html', '.js', '.css', '.img', '.png', '.ico', '.xml', '.json', '.txt'];
+
+function isStatic(resource){
+	var resourceExtn = path.extname(resource);
+	return staticExtns.indexOf(resourceExtn) !== -1;
+}
 
 var server = http.createServer(function(req, res){
 	console.log(req.method, req.url);
-	res.write('<h1>Welcome to node</h1>');
-	res.end();
+	var resourceName = req.url === '/' ? 'index.html' : req.url;
+	var urlObj = url.parse(resourceName);
+	if (isStatic(urlObj.pathname)){
+		var resource = path.join(__dirname, urlObj.pathname);
+		if (fs.existsSync(resource)){
+			fs.createReadStream(resource).pipe(res);
+		} else {
+			res.statusCode = 404;
+			res.end();
+		}
+	} else if (urlObj.pathname === '/calculator'){
+		var reqData = querystring.parse(urlObj.query);
+		var op = reqData.op,
+			n1 = parseInt(reqData.n1, 10),
+			n2 = parseInt(reqData.n2, 10);
+		var result = calculator[op](n1, n2);
+		res.write(result.toString());
+		res.end();
+	
+	} else {
+		res.statusCode = 404;
+		res.end();
+	}
 });
-
 server.listen(8080);
-
-
-/*
-add a index.html
-req.url = / or index.html
-	serve index.html
-else
-	res.statusCode = 404;
-	res.end()
-*/
